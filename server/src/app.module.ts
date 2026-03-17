ffe981f2ef5d2d065d7bdd3c9a68c02b4005403d
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2026-03-17 17:41:11 -0300

    ✨feat: implementar CRUD de disciplinas

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,12 +5,14 @@ import { AppService } from './app.service';
 import { PrismaModule } from './shared/prisma/prisma.module';
 import { UsuariosModule } from './modules/usuarios/usuarios.module';
 import { CursosModule } from './modules/cursos/cursos.module';
+import { DisciplinaModule } from './modules/disciplina/disciplina.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     UsuariosModule,
     CursosModule,
+    DisciplinaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
