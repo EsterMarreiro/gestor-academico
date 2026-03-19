f7cd370d2e25b7a6d7ea8cdb3a2cad22b338892c
GabrielClemente05 <gabclemente.code@gmail.com>
2026-03-19 14:32:54 -0300

    :sparkles: feat: módulo de avaliação

---
+++ server/src/avaliacao/avaliacao.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAvaliacaoDto } from './dto/create-avaliacao.dto';
+import { UpdateAvaliacaoDto } from './dto/update-avaliacao.dto';
+
+@Injectable()
+export class AvaliacaoService {
+  create(createAvaliacaoDto: CreateAvaliacaoDto) {
+    return 'This action adds a new avaliação';
+  }
+
+  findAll() {
+    return `This action returns all avaliação`; 
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} avaliação`;
+  }
+
+  update(id: number, updateAvaliacaoDto: UpdateAvaliacaoDto) {
+    return `This action updates a #${id} avaliação`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} avaliação`;
+  }
+}
