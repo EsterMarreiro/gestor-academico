fc56d8aa493aa5aa688a1e0087f9b7c08becbea6
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2026-03-26 19:44:30 -0300

    ✨feat:adicionar testes unitários do módulo de cursos e disciplinas

--- server/src/modules/disciplina/disciplina.controller.spec.ts
+++ server/src/modules/disciplina/disciplina.controller.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DisciplinaController } from './disciplina.controller';
-
+import { DisciplinaService } from './disciplina.service';
 describe('DisciplinaController', () => {
   let controller: DisciplinaController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DisciplinaController],
+      providers: [DisciplinaService],
     }).compile();
 
     controller = module.get<DisciplinaController>(DisciplinaController);
