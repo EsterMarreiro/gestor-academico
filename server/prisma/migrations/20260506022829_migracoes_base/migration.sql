e49f09b12cb40c5bcdc335e37e25aa8786b795a5
Ester Marreiro <estermarreiro@outlook.com>
2026-05-05 23:37:38 -0300

    chore: :card_file_box: Cria migrações para a modelagem refatorada e realiza as últimas configurações do Docker

---
+++ server/prisma/migrations/20260506022829_migracoes_base/migration.sql
@@ -0,0 +1,206 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Curso` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Disciplina` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Usuario` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "StatusMatricula" AS ENUM ('pendente', 'ativa', 'em_fila', 'recusada');
+
+-- CreateEnum
+CREATE TYPE "StatusInscricaoProfessor" AS ENUM ('pendente', 'aprovada', 'recusada');
+
+-- DropForeignKey
+ALTER TABLE "Disciplina" DROP CONSTRAINT "Disciplina_cursoId_fkey";
+
+-- DropTable
+DROP TABLE "Curso";
+
+-- DropTable
+DROP TABLE "Disciplina";
+
+-- DropTable
+DROP TABLE "Usuario";
+
+-- DropEnum
+DROP TYPE "TipoUsuario";
+
+-- CreateTable
+CREATE TABLE "usuario" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "data_nascimento" DATE NOT NULL,
+    "is_admin" BOOLEAN NOT NULL DEFAULT false,
+    "criado_em" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizado_em" TIMESTAMP(3) NOT NULL,
+    "deletado_em" TIMESTAMP(3),
+
+    CONSTRAINT "usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "aluno" (
+    "id" SERIAL NOT NULL,
+    "usuario_id" INTEGER NOT NULL,
+    "numero_matricula" TEXT NOT NULL,
+    "criado_em" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizado_em" TIMESTAMP(3) NOT NULL,
+    "deletado_em" TIMESTAMP(3),
+
+    CONSTRAINT "aluno_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "professor" (
+    "id" SERIAL NOT NULL,
+    "usuario_id" INTEGER NOT NULL,
+    "titulacao" TEXT,
+    "criado_em" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizado_em" TIMESTAMP(3) NOT NULL,
+    "deletado_em" TIMESTAMP(3),
+
+    CONSTRAINT "professor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "curso" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT,
+    "criado_em" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizado_em" TIMESTAMP(3) NOT NULL,
+    "deletado_em" TIMESTAMP(3),
+
+    CONSTRAINT "curso_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "disciplina" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT,
+    "curso_id" INTEGER NOT NULL,
+    "professor_id" INTEGER,
+    "criado_em" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizado_em" TIMESTAMP(3) NOT NULL,
+    "deletado_em" TIMESTAMP(3),
+
+    CONSTRAINT "disciplina_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "turma" (
+    "id" SERIAL NOT NULL,
+    "codigo" TEXT NOT NULL,
+    "disciplina_id" INTEGER NOT NULL,
+    "vagas_total" INTEGER NOT NULL,
+    "criado_em" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizado_em" TIMESTAMP(3) NOT NULL,
+    "deletado_em" TIMESTAMP(3),
+
+    CONSTRAINT "turma_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "aluno_turma" (
+    "id" SERIAL NOT NULL,
+    "aluno_id" INTEGER NOT NULL,
+    "turma_id" INTEGER NOT NULL,
+    "criado_em" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizado_em" TIMESTAMP(3) NOT NULL,
+    "deletado_em" TIMESTAMP(3),
+
+    CONSTRAINT "aluno_turma_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "matricula" (
+    "id" SERIAL NOT NULL,
+    "aluno_id" INTEGER NOT NULL,
+    "curso_id" INTEGER NOT NULL,
+    "status" "StatusMatricula" NOT NULL DEFAULT 'pendente',
+    "criado_em" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizado_em" TIMESTAMP(3) NOT NULL,
+    "deletado_em" TIMESTAMP(3),
+
+    CONSTRAINT "matricula_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "inscricao_professor" (
+    "id" SERIAL NOT NULL,
+    "usuario_id" INTEGER NOT NULL,
+    "disciplina_id" INTEGER NOT NULL,
+    "status" "StatusInscricaoProfessor" NOT NULL DEFAULT 'pendente',
+    "criado_em" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizado_em" TIMESTAMP(3) NOT NULL,
+    "deletado_em" TIMESTAMP(3),
+
+    CONSTRAINT "inscricao_professor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "usuario_email_key" ON "usuario"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "usuario_cpf_key" ON "usuario"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "aluno_usuario_id_key" ON "aluno"("usuario_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "aluno_numero_matricula_key" ON "aluno"("numero_matricula");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "professor_usuario_id_key" ON "professor"("usuario_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "turma_codigo_key" ON "turma"("codigo");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "aluno_turma_aluno_id_turma_id_key" ON "aluno_turma"("aluno_id", "turma_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "matricula_aluno_id_curso_id_key" ON "matricula"("aluno_id", "curso_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "inscricao_professor_usuario_id_disciplina_id_key" ON "inscricao_professor"("usuario_id", "disciplina_id");
+
+-- AddForeignKey
+ALTER TABLE "aluno" ADD CONSTRAINT "aluno_usuario_id_fkey" FOREIGN KEY ("usuario_id") REFERENCES "usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "professor" ADD CONSTRAINT "professor_usuario_id_fkey" FOREIGN KEY ("usuario_id") REFERENCES "usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "disciplina" ADD CONSTRAINT "disciplina_curso_id_fkey" FOREIGN KEY ("curso_id") REFERENCES "curso"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "disciplina" ADD CONSTRAINT "disciplina_professor_id_fkey" FOREIGN KEY ("professor_id") REFERENCES "professor"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "turma" ADD CONSTRAINT "turma_disciplina_id_fkey" FOREIGN KEY ("disciplina_id") REFERENCES "disciplina"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "aluno_turma" ADD CONSTRAINT "aluno_turma_aluno_id_fkey" FOREIGN KEY ("aluno_id") REFERENCES "aluno"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "aluno_turma" ADD CONSTRAINT "aluno_turma_turma_id_fkey" FOREIGN KEY ("turma_id") REFERENCES "turma"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "matricula" ADD CONSTRAINT "matricula_aluno_id_fkey" FOREIGN KEY ("aluno_id") REFERENCES "aluno"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "matricula" ADD CONSTRAINT "matricula_curso_id_fkey" FOREIGN KEY ("curso_id") REFERENCES "curso"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "inscricao_professor" ADD CONSTRAINT "inscricao_professor_usuario_id_fkey" FOREIGN KEY ("usuario_id") REFERENCES "usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "inscricao_professor" ADD CONSTRAINT "inscricao_professor_disciplina_id_fkey" FOREIGN KEY ("disciplina_id") REFERENCES "disciplina"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
