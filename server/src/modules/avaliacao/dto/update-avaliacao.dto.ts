30225953c8a4ac8042263213b07b45e7014902cf
Ester Marreiro <estermarreiro@outlook.com>
2026-06-04 00:03:00 -0300

    chore: :wrench: padroniza node 22, corrige healthcheck no docker compose e remove de presenca e avaliacao

--- server/src/modules/avaliacao/dto/update-avaliacao.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAvaliacaoDto } from './create-avaliacao.dto';
-
-export class UpdateAvaliacaoDto extends PartialType(CreateAvaliacaoDto) {}
